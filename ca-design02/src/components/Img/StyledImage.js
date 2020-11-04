0d18dd3932e1603b1ef95e99c7da6a0a5e5dd0fd
Farida <farida.alin@yahoo.com>
2020-11-04 14:47:39 +0100

    lazy loading images

---
+++ ca-design02/src/components/Img/StyledImage.js
@@ -0,0 +1,10 @@
+import styled from "styled-components/macro";
+
+export const StyledImage = styled.img`
+
+
+`;
+
+export const Container = styled.div`
+
+`;
---
+++ ca-design02/src/components/Img/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import { StyledImage, Container } from './StyledImage'
+import largeImg from '../../Images/officeImages/cowomen_w_1076.jpg'
+import medImg from '../../Images/officeImages/cowomen_w_645.jpg'
+import smallImg from '../../Images/officeImages/cowomen_w_200.jpg'
+
+const Images = () => (
+  <figure>
+    <picture>
+    <LazyLoadImage
+      alt="women working in a office"
+      effect="blur"
+      src={smallImg}
+      srcSet={`${smallImg} 200w, ${medImg} 645w, ${largeImg} 1076w`} // use normal <img> attributes as props
+       />
+    </picture>
+    <figcaption id="">
+        16:9
+            </figcaption>
+
+  </figure>
+);
+
+export { Images };
