a7b143ba8619e99bba650eab5ddb63d7a2acaddb
Farida <farida.alin@yahoo.com>
2020-11-06 13:05:52 +0100

    added descrption to links

--- ca-design02/src/components/Links/index.js
+++ ca-design02/src/components/Links/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyledInlineLink, StyledIndepLink, Container } from "./StyledLink";
+import Description from "../Description/Description";
 
 function InlineLink(props) {
   return <StyledInlineLink {...props}>{props.children}</StyledInlineLink>;
@@ -10,18 +11,22 @@ function IndependentLink(props) {
 function Links(props) {
   return (
     <Container id={props.id}>
+      <Description>
+       There are two main links in JURK´s design system. Inline links and independent links. The inline links is to used wihtin lenghty text, a sentence or paragraph.
+       Independent links are deault link type, that are can stand alone and should be used as navigation and only with short descriptive text. They can be paired with an icon.
+      </Description>
       <div>
         <InlineLink className="default" default href="">
           Inline link
         </InlineLink>
         <InlineLink className="hover" hover href="">
-        Inline link hovered
+          Inline link hovered
         </InlineLink>
         <InlineLink className="active" active href="">
-        Inline link pressed
+          Inline link pressed
         </InlineLink>
         <InlineLink className="focus" focus href="">
-        Inline link focused
+          Inline link focused
         </InlineLink>
       </div>
       <div>
@@ -29,13 +34,13 @@ function Links(props) {
           Independent link
         </IndependentLink>
         <IndependentLink className="" hover href="">
-        Independent link hovered
+          Independent link hovered
         </IndependentLink>
         <IndependentLink className="" active href="">
-        Independent link pressed
+          Independent link pressed
         </IndependentLink>
         <IndependentLink className="" focus href="">
-        Independent link focused
+          Independent link focused
         </IndependentLink>
       </div>
     </Container>
